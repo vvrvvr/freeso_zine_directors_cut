9d7a866cdc8c876d265ac57ec2cba33b280631eb
v.svistunov <v.svistunov@corp.nekki.ru>
2025-08-13 20:42:22 +0300

    разбил 1 сцену на части и скрипты

---
+++ Portal 2 zine/Assets/Portal2Assets/Scripts/MoveOnEnable.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class MoveOnEnable : MonoBehaviour
+{
+    public float activeY = 0f;       // Позиция по Y при активации
+    public float inactiveY = -5f;    // Позиция по Y при деактивации
+    public float speed = 2f;         // Скорость движения
+
+    private void OnEnable()
+    {
+        StopAllCoroutines();
+        StartCoroutine(MoveToY(activeY));
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+        transform.localPosition = new Vector3(transform.localPosition.x, inactiveY, transform.localPosition.z);
+    }
+
+    private System.Collections.IEnumerator MoveToY(float targetY)
+    {
+        Vector3 pos = transform.localPosition;
+
+        while (Mathf.Abs(pos.y - targetY) > 0.01f)
+        {
+            pos.y = Mathf.MoveTowards(pos.y, targetY, speed * Time.deltaTime);
+            transform.localPosition = pos;
+            yield return null;
+        }
+
+        pos.y = targetY;
+        transform.localPosition = pos;
+    }
+}
